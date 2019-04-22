d2e132a1e348998893ccd9deecb7615c11f90572
Nadin Prokhorova <nadin.prokhorova@gmail.com>
2019-04-22 22:25:51 +0300

    My first commit

---
+++ homework1_task1/src/homework1_task1/task1.java
@@ -0,0 +1,9 @@
+package homework1_task1;
+
+public class task1 {
+		public static void main ( String [] args ) {
+			System.out.println ("Hello my dear");
+			System.out.println ("Nadin");
+		}
+	}
+
---
+++ homework1_task2/src/homework1_task2/task_not.java
@@ -0,0 +1,10 @@
+package homework1_task2;
+
+public class task_not {
+
+	public static void main(String[] args) {
+		System.out.println("Life is good!");
+
+	}
+
+}
