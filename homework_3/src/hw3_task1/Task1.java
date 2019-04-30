33d265087f0a285a8460447c4c4dd919173e84d8
Nadin Prokhorova <nadin.prokhorova@gmail.com>
2019-04-30 15:19:03 +0300

    homework_3

--- homework_3/src/hw3_task1/Task1.java
+++ homework_3/src/hw3_task1/Task1.java
@@ -11,18 +11,23 @@ public class Task1 {
 
     public static void main (String [] args) {
 
-        //System.out.println( args [0]);
+        //System.out.println( args [0]);// узнать значение аргумента
         String num1 = args[0];
-        //System.out.println(num1);
-        int num1new = Integer.parseInt("10");
+        int num1new = Integer.parseInt(num1);
+        //System.out.println(num1new);// проверить правильность присвоенного значения
         int num2 = 256;
+        int sum = 0;
 
-        for (int i = 0; i < num1new ; i++){
+       // num2 = num2 * num1new;// узнать результат с помощью умножения
+        //System.out.println(num2);
 
-             num2 = num2 ;
+
+        for (int i = 0; i < num1new; i++ ) {
+
+            sum += num2;
 
         }
-        System.out.println(num2);
+        System.out.println(sum);
 
     }
 
