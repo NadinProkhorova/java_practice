19432ab852b7f485becbbf6217dd9b94225c526e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-05-03 13:21:06 +0300

    Update MagicBall.java

--- homework_5/src/Task1/MagicBall.java
+++ homework_5/src/Task1/MagicBall.java
@@ -1,3 +1,4 @@
+// имена пакетов с маленькой буквы
 package Task1;
 /*
 Дано: Шар предсказаний.
@@ -11,7 +12,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 class MagicBall {
-
+    private static final String QUESTION_SYMBOL = "?";
 
     public static void main(String[] args) {
 
@@ -19,6 +20,7 @@ class MagicBall {
         System.out.println(str);
         boolean finishProgram = true;
         while (finishProgram) {
+            // не оставляй закоменченный код в уже готовом варианте
             //for (int i = 0; i < 1; i++) {
             Scanner scan = new Scanner(System.in);
             String question = new String();
@@ -32,6 +34,7 @@ class MagicBall {
 
             String symb = "?";
 
+            // этот if проверяет наличие знака вопроса в любом месте, т.е. даже если будет "? твой вопрос", то твой if вернет тру
             if (question.contains(symb) != true) {
                 System.out.println("Задай свой вопрос еще раз, поставь в конце знак  \" ? \"");
                 //i--;// для цикла for
@@ -46,14 +49,25 @@ class MagicBall {
                 }
                 finishProgram = false;
                 //i++;// для цикла for
-
+//здесь абзац (пробел строки) не нужен)
             }
         }
-
-
+//здесь абзац (пробел строки) не нужен)
+//здесь абзац (пробел строки) не нужен)
     }
-
-
+//здесь абзац (пробел строки) не нужен)
+//здесь абзац (пробел строки) не нужен)
+    pubpic static boolean getRandomAnswer() {
+        return new Random().nextBoolean();
+    }
+   
+   private static boolean isQuestion(String questionToCheck) {
+        if (questionToCheck == null) {
+            return false;
+        } else {
+            return questionToCheck.endsWith(QUESTION_SYMBOL);
+        }
+    }
 }
 
 
