b12d31d1a5a83604f2c3e007a684dfec06fb1450
Nadin Prokhorova <nadin.prokhorova@gmail.com>
2019-05-05 21:50:38 +0300

    practice_work_3

--- practice/practice_3/src/Task4.java
+++ practice/practice_3/src/Task4.java
@@ -1,11 +1,16 @@
 /*Дано: double number = 45.5;
 Задание: Вывести на экран только целую часть
 */
+
 public class Task4 {
     public static void main (String [] args){
         double number = 45.5;
         int num = (int)number;
         System.out.println ( num );
+
+        int num2 = (int)Math.round(number);
+        System.out.println(num2);
+
 
     }
 }
